8b922759aab09e2fdb50b996232926ad40d4fb09
charlesdev771 <charlesdev07721@proton.me>
2022-12-06 12:28:40 -0300

    login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:authenticate/home_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,12 +10,61 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final _formkey = GlobalKey<FormState>();
+  final _crailcontroller = TextEditingController();
+  final _senhaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('login'),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: _formkey,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: _crailcontroller,
+              decoration: InputDecoration(
+                label: Text("C-rail"), 
+                hintText: 'nome@email.com'
+                ),
+              ),
+
+            TextField(
+              controller: _senhaController,
+              decoration: InputDecoration(
+                label: Text('Senha'),
+                hintText: "sua senha",
+              ),
+            ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formkey.currentState!.validate())
+                  {
+                    logar(); 
+                  }
+                },
+                child: Text('Login')
+              ),
+          ],
+        ),
       )
+      ),
     );
   }
+
+  logar() async 
+  {
+    Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ), 
+    );
+  }
+
+
 }
